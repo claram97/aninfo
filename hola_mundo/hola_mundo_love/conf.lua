2aca80a754883135d6e9ac433f24a25c00486872
Clara Ruano Frugoli <cruano@fi.uba.ar>
2023-11-17 18:47:13 -0300

    Add files via upload

---
+++ hola_mundo/hola_mundo_love/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.window.title = "Hola Mundo en Love2D"
+    t.window.width = 400
+    t.window.height = 400
+end
---
+++ hola_mundo/hola_mundo_love/main.lua
@@ -0,0 +1,3 @@
+function love.draw()
+    love.graphics.print("¡Hola, mundo!", 150, 200)
+end
