3e975cba55dd851e3c53fe09395c41e8d20c03d4
ValentinSavarese <86436077+ValentinSavarese@users.noreply.github.com>
2023-12-05 16:42:10 -0300

    agrego la logica de los botones

--- pantallas_modo_clasico/main.lua
+++ pantallas_modo_clasico/main.lua
@@ -1,5 +1,22 @@
-local modoClasico = require("pantalla_modo_clasico")
+-- main.lua
+
+FuncionesExtras = require("pantalla_modo_clasico")
+
+
+function love.load()
+    -- Configuraciones iniciales, si es necesario
+end
+
+function love.update(dt)
+    -- Actualizaciones lógicas, si es necesario
+end
 
 function love.draw()
-    modoClasico.dibujar()
+    -- Dibuja la pantalla de inicio
+    FuncionesExtras.mostrarPantallaInicio()
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    -- Maneja los clics del mouse
+    FuncionesExtras.mousepressed(x, y, button, istouch, presses)
 end
--- pantallas_modo_clasico/pantalla_modo_clasico.lua
+++ pantallas_modo_clasico/pantalla_modo_clasico.lua
@@ -1,28 +1,70 @@
+local function dibujarFondo()
+    love.graphics.setBackgroundColor(0.96, 0.86, 0.69)  -- Cambiado a un tono de beige
+end
 
-local modoClasico = {}
-
-function modoClasico.dibujar()
-    love.graphics.setBackgroundColor(1, 1, 0.8)
-
+local function dibujarTexto()
     love.graphics.setColor(0, 0, 0)
-    love.graphics.setFont(love.graphics.newFont(60))
-    love.graphics.printf("Modo Clásico", 0, 50, love.graphics.getWidth(), "center")
+    
+    -- Almacenar la fuente original
+    local originalFont = love.graphics.getFont()
 
-    local offsetY = 100
+    
+    local fontSize = 80  -- cambiar fuentee
+    local largeFont = love.graphics.newFont(fontSize)
+    love.graphics.setFont(largeFont)
 
+    love.graphics.printf("Modo Clásico", 0, 100, love.graphics.getWidth(), "center")
+
+    love.graphics.setFont(originalFont)
+end
+
+local function dibujarBotones()
     local buttonWidth = 220
     local buttonHeight = 50
+    local cornerRadius = 15
+    local jugador1ButtonX = love.graphics.getWidth() / 4
+    local jugador2ButtonX = 3 * love.graphics.getWidth() / 4
+    local buttonY = 400
 
-    modoClasico.dibujarBoton(love.graphics.getWidth() / 4, 200 + offsetY, buttonWidth, buttonHeight, "1 Jugador")
-    modoClasico.dibujarBoton(3 * love.graphics.getWidth() / 4, 200 + offsetY, buttonWidth, buttonHeight, "2 Jugadores")
+    love.graphics.setColor(0.2, 0.7, 0.2)  
+    love.graphics.rectangle("fill", jugador1ButtonX - buttonWidth / 2, buttonY, buttonWidth, buttonHeight, cornerRadius, cornerRadius)
+
+    love.graphics.setColor(1, 1, 1)  
+    love.graphics.printf("Jugador 1", jugador1ButtonX - buttonWidth / 2, buttonY + 10, buttonWidth, "center")
+
+    love.graphics.setColor(0.2, 0.7, 0.2)  
+    love.graphics.rectangle("fill", jugador2ButtonX - buttonWidth / 2, buttonY, buttonWidth, buttonHeight, cornerRadius, cornerRadius)
+
+    love.graphics.setColor(1, 1, 1)  
+    love.graphics.printf("Jugador 2", jugador2ButtonX - buttonWidth / 2, buttonY + 10, buttonWidth, "center")
 end
 
-function modoClasico.dibujarBoton(x, y, width, height, texto)
-    love.graphics.setColor(0.2, 0.7, 0.2)
-    love.graphics.rectangle("fill", x - width / 2, y, width, height, 15, 15)
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.setFont(love.graphics.newFont(25))
-    love.graphics.printf(texto, x - width / 2, y + 15, width, "center")
+local function manejarClic(x, y, button)
+    local buttonWidth = 220
+    local buttonHeight = 50
+    local jugador1ButtonX = love.graphics.getWidth() / 4
+    local jugador2ButtonX = 3 * love.graphics.getWidth() / 4
+    local buttonY = 400
+
+    if button == 1 and x >= jugador1ButtonX - buttonWidth / 2 and x <= jugador1ButtonX + buttonWidth / 2 and y >= buttonY and y <= buttonY + buttonHeight then
+        -- charo aca agrega aquí la lógica del boton jugador 1
+        print("Clic en Jugador 1") --esto es un ejemplo que puse para ver si andaba
+    elseif button == 1 and x >= jugador2ButtonX - buttonWidth / 2 and x <= jugador2ButtonX + buttonWidth / 2 and y >= buttonY and y <= buttonY + buttonHeight then
+        -- charo aca agrega aquí la lógica del boton jugadores 2
+        print("Clic en Jugador 2") --idem arriba
+    end
 end
 
-return modoClasico
+local FuncionesExtras = {}
+
+function FuncionesExtras.mostrarPantallaInicio()
+    dibujarFondo()
+    dibujarTexto()
+    dibujarBotones()
+end
+
+function FuncionesExtras.mousepressed(x, y, button, istouch, presses)
+    manejarClic(x, y, button)
+end
+
+return FuncionesExtras
