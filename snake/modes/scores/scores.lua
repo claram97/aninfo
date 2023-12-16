fb285a71f657a260aa661a6b1ee10d98299c4471
claram97 <cruano@fi.uba.ar>
2023-12-16 01:46:19 -0300

    formato scores

--- snake/modes/scores/scores.lua
+++ snake/modes/scores/scores.lua
@@ -37,36 +37,6 @@ function scores.readCsv()
     return datos
 end
 
--- function scores.readCsv()
---     local file = io.open(".scores", "r")
---     local datos = {}  -- Tabla para almacenar los datos del CSV
-
---     if file then
---         for linea in file:lines() do
---             local campos = {}  -- Tabla para almacenar los campos de cada línea
---             for valor in string.gmatch(linea, "[^;]+") do
---                 table.insert(campos, valor)  -- Agrega cada campo a la tabla
---             end
-
---             -- Verifica que la línea tenga al menos 3 campos antes de intentar acceder a ellos
---             if #campos >= 3 then
---                 table.insert(datos, {
---                     nombre = campos[1],
---                     puntuacion = tonumber(campos[2]),
---                     modo = campos[3],  -- Agrega el campo de modo
---                     fecha = campos[4]  -- Agrega el campo de fecha
---                 })
---             else
---                 -- Manejo de error o advertencia si la línea no tiene suficientes campos
---                 print("Advertencia: La línea no tiene suficientes campos.")
---             end
---         end
-
---         file:close()  -- Cierra el archivo
---     end
---     return datos
--- end
-
 function split(str, delimiter)
     local result = {}
     local pattern = "(.-)" .. delimiter
@@ -144,62 +114,6 @@ function scores.writeCsv(nombre, puntuacion, modo)
     end
 end
 
--- function scores.writeCsv(nombre, puntuacion, modo)
---     -- Abrir el archivo CSV en modo de lectura
---     local file = io.open(".scores", "r")
-
---     if file then
---         local tempFileName = ".scores_temp"
---         local tempFile = io.open(tempFileName, "w")
-
---         if tempFile then
---             local inserted = false
---             local fecha = os.date("%d/%m/%Y %H:%M:%S")
---             local nuevaLinea = nombre .. ";" .. puntuacion .. ";" .. fecha
-            
---             for line in file:lines() do
---                 local result = split(line, ";")
---                 local score = result[2]
---                 if not inserted and puntuacion > tonumber(score) then
---                     tempFile:write(nuevaLinea .. "\n")
---                     inserted = true
---                 end
---                 tempFile:write(line .. "\n")
---             end
-
---             if not inserted then
---                 tempFile:write(nuevaLinea .. "\n")
---             end
-
---             file:close()
---             tempFile:close()
-
---             os.remove(".scores")
---             os.rename(tempFileName, ".scores")
-
---         else
---             print("Error al abrir el archivo temporal.")
---         end
---     else
---         -- El archivo no existe, haz algo aquí si es necesario
---         local newFile = io.open(".scores", "w")
-
---         if newFile then
---             -- El archivo se creó con éxito, haz algo aquí si es necesario
---             -- Agregar la nueva línea con el nombre, puntuación y fecha
---             local fecha = os.date("%d/%m/%Y %H:%M:%S")
---             local nuevaLinea = nombre .. ";" .. puntuacion .. ";" .. fecha
---             newFile:write(nuevaLinea .. "\n")
---             -- Cierra el archivo después de realizar las operaciones necesarias
---             newFile:close()
---         else
---             -- Hubo un error al crear el archivo, maneja el error según sea necesario
---             print("Error al crear el archivo.")
---         end
---     end
--- end
-
-
 function scores.establecerFondo()
     love.graphics.setBackgroundColor(0.95, 0.95, 0.9)
     love.graphics.setColor(251/255, 180/255, 69/255, 100)
@@ -239,9 +153,9 @@ function scores.draw()
     love.graphics.setColor(0, 0, 0)  -- Establecer color de texto a negro
     
     for _, persona in ipairs(datos) do
-        love.graphics.print("Nombre: " .. persona.nombre .. ", Puntuacion: " .. persona.puntuacion .. ", Modo: " .. persona.modo .. ", Fecha: " .. persona.fecha, 120, y)
+        love.graphics.print(persona.nombre .. ", " .. persona.puntuacion .. " puntos, modo " .. persona.modo .. ", " .. persona.fecha, 120, y)
         y = y + 45  -- Incrementar la posición Y para la siguiente línea
-    end
+    end    
 end
 
 function scores.load()
